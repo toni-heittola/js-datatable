4118aa0ef9ca27387e52a88a777027897db42e2f
heittolt <toni.heittola@gmail.com>
2016-09-27 12:30:58 +0300

    Fixing minified css files

--- minification.py
+++ minification.py
@@ -11,9 +11,10 @@ This plugin will create dynamic datatable with charting features from given yaml
 
 import os
 import sys
+import io
 import argparse
 import textwrap
-
+from IPython import embed
 __version_info__ = ('0', '1', '0')
 __version__ = '.'.join(__version_info__)
 
@@ -33,13 +34,13 @@ def main(argv):
     print "JS and CSS minification"
     print "-----------------------"
 
-    minify_css_directory(source='css', target='css.min')
+    minify_css_directory2(source='css', target='css.min')
     minify_js_directory(source='js', target='js.min')
 
 
 def minify_css_directory(source, target):
     """
-    Move CSS resources from source directory to target directory and minify.
+    Move CSS resources from source directory to target directory and minify. Using csscompressor.
 
     """
     from csscompressor import compress
@@ -56,6 +57,26 @@ def minify_css_directory(source, target):
                     with open(current_file_path) as css_file:
                         with open(os.path.join(target, current_file.replace('.css', '.min.css')), "w") as minified_file:
                             minified_file.write(compress(css_file.read()))
+
+def minify_css_directory2(source, target):
+    """
+    Move CSS resources from source directory to target directory and minify. Using rcssmin.
+
+    """
+    import rcssmin
+
+    if os.path.isdir(source):
+        if not os.path.exists(target):
+            os.makedirs(target)
+
+        for root, dirs, files in os.walk(source):
+            for current_file in files:
+                if current_file.endswith(".css"):
+                    current_file_path = os.path.join(root, current_file)
+                    print " ", current_file_path
+                    with open(current_file_path) as css_file:
+                        with open(os.path.join(target, current_file.replace('.css', '.min.css')), "w") as minified_file:
+                            minified_file.write(rcssmin.cssmin(css_file.read(), keep_bang_comments=True))
 
 
 def minify_js_directory(source, target):
