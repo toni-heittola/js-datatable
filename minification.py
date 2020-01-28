796ed731314c8a82e495b999c7e15c5e7d971d8d
toni.heittola@gmail.com <toni.heittola@gmail.com>
2020-01-29 00:17:01 +0200

    Add bundle ordering

--- minification.py
+++ minification.py
@@ -31,11 +31,21 @@ def main(argv):
         '''))
     parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
     args = parser.parse_args()
-    print "JS and CSS minification"
-    print "-----------------------"
+    print("JS and CSS minification")
+    print("-----------------------")
 
     minify_css_directory2(source='css', target='css.min')
-    minify_js_directory(source='js', target='js.min')
+
+    bundle_order = [
+        'bootstrap-table.min.js',
+        'bootstrap-table-filter-control.min.js',
+        'Chart.bundle.min.js',
+        'moment.min.js',
+        'js-yaml.min.js',
+        'datatable.min.js'
+    ]
+
+    minify_js_directory(source='js', target='js.min', bundle_order=bundle_order)
 
 
 def minify_css_directory(source, target):
@@ -66,6 +76,7 @@ def minify_css_directory2(source, target):
     import rcssmin
 
     if os.path.isdir(source):
+        print('CSS processing')
         if not os.path.exists(target):
             os.makedirs(target)
 
@@ -73,7 +84,7 @@ def minify_css_directory2(source, target):
             for current_file in files:
                 if current_file.endswith(".css"):
                     current_file_path = os.path.join(root, current_file)
-                    print(" " + current_file_path)
+                    print("[MINIFY]  " + current_file_path)
                     with open(current_file_path) as css_file:
                         with open(os.path.join(target, current_file.replace('.css', '.min.css')), "w") as minified_file:
                             minified_file.write(rcssmin.cssmin(css_file.read(), keep_bang_comments=True))
@@ -92,9 +103,10 @@ def minify_css_directory2(source, target):
         bundle_file.write(''.join(bundle_data))
         bundle_file.close()
 
-        print(" " + bundle_filename)
+        print("[BUNDLE]  " + bundle_filename)
+        print(" ")
 
-def minify_js_directory(source, target):
+def minify_js_directory(source, target, bundle_order=None):
     """
     Move JS resources from source directory to target directory and minify.
 
@@ -102,6 +114,7 @@ def minify_js_directory(source, target):
     from jsmin import jsmin
 
     if os.path.isdir(source):
+        print('JS minification')
         if not os.path.exists(target):
             os.makedirs(target)
 
@@ -109,26 +122,46 @@ def minify_js_directory(source, target):
             for current_file in files:
                 if current_file.endswith(".js"):
                     current_file_path = os.path.join(root, current_file)
-                    print(" " + current_file_path)
+                    print("[MINIFY]  " + current_file_path)
                     with open(current_file_path) as js_file:
                         with open(os.path.join(target, current_file.replace('.js', '.min.js')), "w") as minified_file:
                             minified_file.write(jsmin(js_file.read()))
 
+        print('JS bundle')
         bundle_data = []
-        for root, dirs, files in os.walk(target):
-            for current_file in files:
-                if current_file.endswith(".js") and current_file !=  'datatable.bundle.min.js':
-                    current_file_path = os.path.join(root, current_file)
-                    js_file = open(current_file_path, "r")
-                    bundle_data += js_file.readlines()
-                    js_file.close()
+        if bundle_order:
+            bundle_order_ = []
+            for item in bundle_order:
+                bundle_order_.append(
+                    os.path.join(target,item)
+                )
+
+            for current_file in bundle_order:
+                current_file_path = os.path.join(target, current_file)
+                print("[BUNDLE]  " + current_file_path)
+
+                js_file = open(current_file_path, "r")
+                bundle_data += js_file.readlines()
+                js_file.close()
+
+        else:
+            for root, dirs, files in os.walk(target):
+                for current_file in files:
+                    if current_file.endswith(".js") and current_file !=  'datatable.bundle.min.js':
+                        current_file_path = os.path.join(root, current_file)
+                        print("[BUNDLE]  " + current_file_path)
+
+                        js_file = open(current_file_path, "r")
+                        bundle_data += js_file.readlines()
+                        js_file.close()
 
         bundle_filename = os.path.join(target, 'datatable.bundle.min.js')
         bundle_file = open(bundle_filename, 'w+')
         bundle_file.write(''.join(bundle_data))
         bundle_file.close()
 
-        print(" " + bundle_filename)
+        print("[WRITE]   " + bundle_filename)
+        print(" ")
 
 if __name__ == "__main__":
     try:
